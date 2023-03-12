226d47519a22cee38db6b284f5cfe17c535e6cd7
Jahid <jahid4k@gmail.com>
2023-03-12 08:37:57 +0600

    initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const ROOT = 5055;
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({ success: true });
+});
+
+app.listen(ROOT, () => {
+  console.log(`Server is running on ${ROOT}`);
+});
